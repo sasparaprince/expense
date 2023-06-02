944df35aea4ceda7df9c231cc62fd5f1294781b2
prince saspara <princesaspara46@gmail.com>
2023-06-02 21:08:35 +0530

    Add props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
-import './ExpenseItem.css'
-function ExpenseItem() {
-  const expanseDate = new Date(2021 , 2 , 28);
-  const expanseTitle = 'car insurance';
-  const expansePrice = 200
+import './ExpenseItem.css';
 
+function ExpenseItem(props) {
   return (
-    <>
-    <div className="expense-item">
-    <div className='expense-item__date'>{expanseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expanseTitle}</h2>
-        <div className="expense-item__price">${expansePrice}</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
-     
-    </>
   );
 }
+
 export default ExpenseItem;
